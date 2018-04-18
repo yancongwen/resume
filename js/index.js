b9acffbe93ae0877a41373ee39e9e114d73b238b
congwenyan <ycwen1119@163.com>
2018-04-18 18:24:46 +0800

    add all

---
+++ js/index.js
@@ -0,0 +1,11 @@
+ all.onclick = function () {
+     portfolioBar.className = 'navBar state-1'
+ }
+
+frame.onclick = function () {
+      portfolioBar.className = 'navBar state-2'
+  }
+
+native.onclick = function () {
+       portfolioBar.className = 'navBar state-3'
+   }
