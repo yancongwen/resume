faa603aa611442db8e2283a3b1a332d5f2caef16
yancongwen <ycwen1119@163.com>
2018-10-14 19:10:35 +0800

    feat: 添加页面加载完成前动画；添加header sticky 效果

--- js/index.js
+++ js/index.js
@@ -1,11 +1,23 @@
- all.onclick = function () {
-     portfolioBar.className = 'navBar state-1'
- }
+all.onclick = function () {
+    portfolioBar.className = 'navBar state-1'
+}
 
 frame.onclick = function () {
-      portfolioBar.className = 'navBar state-2'
-  }
+    portfolioBar.className = 'navBar state-2'
+}
 
 native.onclick = function () {
-       portfolioBar.className = 'navBar state-3'
-   }
+    portfolioBar.className = 'navBar state-3'
+}
+
+window.onscroll = function() {
+    if (window.scrollY > 0) {
+        header.classList.add('sticky')
+    } else {
+        header.classList.remove('sticky')
+    }
+}
+
+setTimeout(function(){
+    loading.style.display = 'none'
+},1500)
