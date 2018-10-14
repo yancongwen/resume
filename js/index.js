42371f8ad8f334eb4c039d81ceb37449056f8bd4
yancongwen <ycwen1119@163.com>
2018-10-14 22:06:08 +0800

    feat: 页面首部导航栏添加CSS动画效果，添加子菜单，js实现导航

--- js/index.js
+++ js/index.js
@@ -18,6 +18,28 @@ window.onscroll = function() {
     }
 }
 
+// 导航栏绑定事件
+nav.childNodes.forEach(function(item){
+    if (item.nodeName === 'LI') {
+        item.onmouseover = function(e) {
+            item.classList.add('active', 'hover')
+        }
+        item.onmouseleave = function(e) {
+            item.classList.remove('active', 'hover')
+        }
+        item.children[0].onclick = function(e) {
+            e.preventDefault()
+            var href = e.target.getAttribute('href')
+            if (href && href !== '#') {
+                var targetDom = document.querySelector(href)
+                if (targetDom) {
+                    scrollTo(0, targetDom.offsetTop)
+                }
+            }
+        }
+    }
+});
+
 setTimeout(function(){
     loading.style.display = 'none'
-},1500)
+},1)
