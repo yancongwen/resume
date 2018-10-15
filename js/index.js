d056e8408105b5220c2a9395fd176dad1bcee007
yancongwen <ycwen1119@163.com>
2018-10-15 10:46:46 +0800

    feat: 借助 tween.js 实现导航栏动画

--- js/index.js
+++ js/index.js
@@ -22,24 +22,43 @@ window.onscroll = function() {
 nav.childNodes.forEach(function(item){
     if (item.nodeName === 'LI') {
         item.onmouseover = function(e) {
-            item.classList.add('active', 'hover')
+            item.classList.add('active')
         }
         item.onmouseleave = function(e) {
-            item.classList.remove('active', 'hover')
+            item.classList.remove('active')
         }
         item.children[0].onclick = function(e) {
             e.preventDefault()
             var href = e.target.getAttribute('href')
             if (href && href !== '#') {
                 var targetDom = document.querySelector(href)
-                if (targetDom) {
-                    scrollTo(0, targetDom.offsetTop)
+                if (!targetDom) {
+                    return
                 }
+                var targetY = targetDom.offsetTop
+                var currentY = window.scrollY
+                // 根据距离设置时间，按照速度 2000px/s 来计算
+                var time = Math.abs(targetY-currentY)/2
+                var coords = { y: currentY }
+                // 借助TWEEN函数库来实现动画函数
+                var tween = new TWEEN.Tween(coords)
+                    .to({y: targetY}, time)
+                    .easing(TWEEN.Easing.Cubic.InOut)
+                    .onUpdate(function() {
+                        window.scrollTo(0, coords.y)
+                    })
+                    .start()
             }
         }
     }
 });
 
+function animate(time) {
+    requestAnimationFrame(animate);
+    TWEEN.update(time);
+}
+requestAnimationFrame(animate);
+
 setTimeout(function(){
     loading.style.display = 'none'
-},1)
+},1000)
